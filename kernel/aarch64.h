cc0885932ccf26fb424483419799f0348066a18b
David Filip <davidfilip@gmail.com>
2025-06-26 10:53:44 +0200

    [codestyle] Fix function code style

--- kernel/aarch64.h
+++ kernel/aarch64.h
@@ -1,24 +1,18 @@
 #ifndef __ASSEMBLER__
 
 // which core is this?
-static inline uint64
-cpuid()
-{
+static inline uint64 cpuid() {
   uint64 x;
   asm volatile("mrs %0, mpidr_el1" : "=r" (x) );
   return x & 0xff;
 }
 
 // Vector Base Address Register in EL1
-static inline void
-w_vbar_el1(uint64 x)
-{
+static inline void w_vbar_el1(uint64 x) {
   asm volatile("msr vbar_el1, %0" : : "r" (x) );
 }
 
-static inline uint64
-r_vbar_el1()
-{
+static inline uint64 r_vbar_el1() {
   uint64 x;
   asm volatile("mrs %0, vbar_el1" : "=r" (x) );
   return x;
@@ -27,171 +21,125 @@ r_vbar_el1()
 // TODO: fix comment
 // supervisor address translation and protection;
 // holds the address of the page table.
-static inline void
-w_ttbr0_el1(uint64 x)
-{
+static inline void w_ttbr0_el1(uint64 x) {
   asm volatile("msr ttbr0_el1, %0" : : "r" (x) );
 }
 
-static inline uint64
-r_ttbr0_el1()
-{
+static inline uint64 r_ttbr0_el1() {
   uint64 x;
   asm volatile("mrs %0, ttbr0_el1" : "=r" (x) );
   return x;
 }
 
-static inline void
-w_ttbr1_el1(uint64 x)
-{
+static inline void w_ttbr1_el1(uint64 x) {
   asm volatile("msr ttbr1_el1, %0" : : "r" (x));
 }
 
-static inline uint64
-r_ttbr1_el1()
-{
+static inline uint64 r_ttbr1_el1() {
   uint64 x;
   asm volatile("mrs %0, ttbr1_el1" : "=r" (x) );
   return x;
 }
 
-static inline uint64
-r_esr_el1()
-{
+static inline uint64 r_esr_el1() {
   uint64 x;
   asm volatile("mrs %0, esr_el1" : "=r" (x) );
   return x;
 }
 
-static inline void
-w_esr_el1(uint64 x)
-{
+static inline void w_esr_el1(uint64 x) {
   asm volatile("msr esr_el1, %0" : : "r" (x) );
 }
 
-static inline uint64
-r_elr_el1()
-{
+static inline uint64 r_elr_el1() {
   uint64 x;
   asm volatile("mrs %0, elr_el1" : "=r" (x) );
   return x;
 }
 
-static inline uint64
-r_far_el1()
-{
+static inline uint64 r_far_el1() {
   uint64 x;
   asm volatile("mrs %0, far_el1" : "=r" (x) );
   return x;
 }
 
 // armv8 generic timer
-static inline uint64
-r_cntv_ctl_el0()
-{
+static inline uint64 r_cntv_ctl_el0() {
   uint64 x;
   asm volatile("mrs %0, cntv_ctl_el0" : "=r" (x) );
   return x;
 }
 
-static inline void
-w_cntv_ctl_el0(uint64 x)
-{
+static inline void w_cntv_ctl_el0(uint64 x) {
   asm volatile("msr cntv_ctl_el0, %0" : : "r" (x) );
 }
 
-static inline uint64
-r_cntv_tval_el0()
-{
+static inline uint64 r_cntv_tval_el0() {
   uint64 x;
   asm volatile("mrs %0, cntv_tval_el0" : "=r" (x) );
   return x;
 }
 
-static inline void
-w_cntv_tval_el0(uint64 x)
-{
+static inline void w_cntv_tval_el0(uint64 x) {
   asm volatile("msr cntv_tval_el0, %0" : : "r" (x) );
 }
 
-static inline uint64
-r_cntvct_el0()
-{
+static inline uint64 r_cntvct_el0() {
   uint64 x;
   asm volatile("mrs %0, cntvct_el0" : "=r" (x) );
   return x;
 }
 
-static inline uint64
-r_cntfrq_el0()
-{
+static inline uint64 r_cntfrq_el0() {
   uint64 x;
   asm volatile("mrs %0, cntfrq_el0" : "=r" (x) );
   return x;
 }
 
 // enable device interrupts(irq)
-static inline void
-intr_on()
-{
+static inline void intr_on() {
   asm volatile("msr daifclr, #0xf" ::: "memory");
 }
 
 // disable device interrupts(irq)
-static inline void
-intr_off()
-{
+static inline void intr_off() {
   asm volatile("msr daifset, #0xf" ::: "memory");
 }
 
-static inline uint64
-daif()
-{
+static inline uint64 daif() {
   uint64 x;
   asm volatile("mrs %0, daif" : "=r" (x) );
   return x;
 }
 
 // are device interrupts(irq) enabled?
-static inline int
-intr_get()
-{
+static inline int intr_get() {
   uint64 x = daif();
   return ((x >> 6) & 0x2) == 0;
 }
 
-static inline uint64
-r_sp()
-{
+static inline uint64 r_sp() {
   uint64 x;
   asm volatile("mov %0, sp" : "=r" (x) );
   return x;
 }
 
-static inline uint64
-r_ra()
-{
+static inline uint64 r_ra() {
   uint64 x;
   asm volatile("mov %0, x30" : "=r" (x) );
   return x;
 }
 
-static inline void
-isb()
-{
+static inline void isb() {
   asm volatile("isb");
 }
 
-static inline void
-dsb()
-{
+static inline void dsb() {
   asm volatile("dsb sy");
 }
 
 // flush the TLB.
-static inline void
-flush_tlb()
-{
+static inline void flush_tlb() {
   asm volatile("dsb ishst");
   asm volatile("tlbi vmalle1is");
   asm volatile("dsb ish");
